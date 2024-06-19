a92d5a9b991a568798ac9827196d135033cb4ece
Matt Hunzinger <matthunz2@gmail.com>
2024-06-19 13:38:25 -0400

    Create Format type

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@
 -- License     : Apache-2.0
 module Main where
 
+import Data.Char (toLower)
 import Data.Int (Int8)
 import Data.Word (Word8)
 import Midi (MidiNote (..), fretMidi, octave, pitch, standardTuning)
@@ -14,20 +15,38 @@ import qualified Text.Parsec.Error
 import qualified Text.Parsec.String
 import Text.Read (readEither)
 
-data Command = Default | Frets
+data Format = Note | Fret | Midi deriving (Show, Eq)
 
 data Args = Args
-  {fretsFlagA :: Bool, transposeA :: Int8}
+  { transposeA :: Int8,
+    inputA :: Format,
+    outputA :: Format
+  }
+
+formatParser :: Char -> String -> String -> Parser Format
+formatParser c l h =
+  option
+    parseFormat
+    ( short c
+        <> long l
+        <> metavar "FRET | NOTE | MIDI"
+        <> help h
+        <> showDefault
+        <> value Note
+    )
+  where
+    parseFormat = do
+      s <- str
+      case map toLower s of
+        "fret" -> return Fret
+        "note" -> return Note
+        "midi" -> return Midi
+        _ -> fail "Invalid input type. Use 'fret' or 'note'."
 
 argsParser :: Parser Args
 argsParser =
   Args
-    <$> switch
-      ( long "frets"
-          <> short 'f'
-          <> help "Input frets"
-      )
-    <*> option
+    <$> option
       auto
       ( short 't'
           <> long "transpose"
@@ -36,6 +55,8 @@ argsParser =
           <> value 0
           <> metavar "HALFSTEPS"
       )
+    <*> formatParser 'i' "input" "Input format"
+    <*> formatParser 'o' "output" "Output format"
 
 main :: IO ()
 main = runFrets =<< execParser opts
@@ -44,20 +65,26 @@ main = runFrets =<< execParser opts
 
 runFrets :: Args -> IO ()
 runFrets args = do
-  frets <- getLine
-  let notes =
-        if fretsFlagA args
-          then case parseFrets frets of
-            Left e -> error (show e)
-            Right xs -> zipWith fretMidi xs standardTuning
-          else case parseNotes frets of
-            Left e -> error (show e)
-            Right xs -> xs
+  line <- getLine
+  let notes = case inputA args of
+        Fret -> case parseInts line of
+          Left e -> error (show e)
+          Right xs -> zipWith fretMidi xs standardTuning
+        Note -> case parseNotes line of
+          Left e -> error (show e)
+          Right xs -> xs
+        Midi -> case parseInts line of
+          Left e -> error (show e)
+          Right xs -> map MidiNote xs
       output = map (\(MidiNote n) -> MidiNote (fromIntegral (fromIntegral n + transposeA args))) notes
    in putStrLn $
         unwords $
           map
-            (\note -> show (pitch note) ++ show (octave note))
+            ( \note -> case outputA args of
+                Note -> show (pitch note) ++ show (octave note)
+                Fret -> error "TODO"
+                Midi -> let (MidiNote n) = note in show n
+            )
             output
 
 notesParser :: Text.Parsec.String.Parser [MidiNote]
@@ -66,5 +93,5 @@ notesParser = sepBy Midi.parse (char ' ')
 parseNotes :: String -> Either Text.Parsec.Error.ParseError [MidiNote]
 parseNotes = parse notesParser ""
 
-parseFrets :: String -> Either String [Word8]
-parseFrets s = mapM readEither (words s)
+parseInts :: String -> Either String [Word8]
+parseInts s = mapM readEither (words s)
